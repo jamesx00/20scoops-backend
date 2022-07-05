cdfb3fa9d292100662cd668f9aed561a36db1230
Siwat Tansiri <tansirijames@gmail.com>
2022-07-05 15:30:16 +0700

    feat: Add data validation

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,11 +1,33 @@
-export interface CreateUserDto {
-  id: string;
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   identificationNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @IsString()
+  @IsOptional()
   history?: string;
-  dateCreated: Date;
-  dateUpdated: Date;
-  deleted: boolean;
-  dateDeleted?: Date;
+}
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  history?: string;
 }
