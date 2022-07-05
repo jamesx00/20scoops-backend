2265fc5f5e3ebaf873d0f92b9e5b483496a383a4
Siwat Tansiri <tansirijames@gmail.com>
2022-07-05 12:48:30 +0700

    feat: Add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      process.env.DATABASE_URL,
+    ),
+  ],
 })
 export class AppModule {}
