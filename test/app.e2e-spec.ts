078ebbe00b1041fe0b42908c7c90398a2d248b43
Siwat Tansiri <tansirijames@gmail.com>
2022-07-05 21:39:57 +0700

    chore: Remove unused functions

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -346,22 +346,3 @@ describe('AppController (e2e)', () => {
     await mongoose.connection.close();
   });
 });
-
-function makeId(length: number): string {
-  let result = '';
-
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  const charactersLength = characters.length;
-
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(
-      Math.floor(
-        Math.random() * charactersLength,
-      ),
-    );
-  }
-
-  return result;
-}
